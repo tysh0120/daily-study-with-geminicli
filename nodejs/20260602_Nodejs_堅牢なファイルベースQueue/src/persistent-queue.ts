b4badfce05ec2a4244de9643f45348b0a00ca26c
Tsuyoshi Kimura <tuyosinyo@gmail.com>
2026-06-07 09:42:06 +0900

    feature: purge機能追加

--- nodejs/20260602_Nodejs_堅牢なファイルベースQueue/src/persistent-queue.ts
+++ nodejs/20260602_Nodejs_堅牢なファイルベースQueue/src/persistent-queue.ts
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs/promises';
+import { finished } from 'node:stream/promises';
 
 export class QueueEmptyError extends Error {}
 export class DuplicateNameError extends Error {}
@@ -11,6 +12,11 @@ export class PersistentQueue<T> {
     private _spos: number; 
     private _epos: number;
 
+    /**
+     * @private
+     * @param {string} name
+     * @param {string _queueDir
+     */
     private constructor(private name: string, private _queueDir: string='./queues') {
         if (PersistentQueue.allQueueNames.has(name)) {
             throw new DuplicateNameError(`${name} は既に使用されてます`);
@@ -22,12 +28,22 @@ export class PersistentQueue<T> {
         [this._spos, this._epos] = [0, 0];
     }
 
+    /**
+     * ファクトリメソッド
+     * @param {string} name
+     * @param {string} queueDir: キューを作成するディレクトリを指定
+     * @returns PersistentQueue<T>
+     */
     static async create(name: string, queueDir='./queues') {
         const persistentQueue = new PersistentQueue(name, queueDir);
         await persistentQueue.recovery();
         return persistentQueue;
     }
 
+    /**
+     * enqueue
+     * @param {T} val
+     */
     async enqueue(value: T): Promise<void> {
         let handle;
         try {
@@ -45,6 +61,10 @@ export class PersistentQueue<T> {
         }
     }
    
+    /**
+     * dequeue
+     * @returns {T}
+     */
     async dequeue(): Promise<T> {
         if (this._queue.length == 0) {
             throw new QueueEmptyError('キューが空です');
@@ -58,14 +78,25 @@ export class PersistentQueue<T> {
         return this._queue.shift()!;
     }
 
+    /**
+     * 先頭要素を返却
+     * @returns {T}
+     */
     peek(): T | undefined {
         return this._queue[0];
     }
 
+    /**
+     * サイズ
+     * @returns {number}
+     */
     size(): number {
         return this._queue.length;
     }
     
+    /**
+     * ファイルより前回終了時の状態復帰
+     */
     async recovery(): Promise<void> {
         // キュー管理用ディレクトリ作成(あればそのまま利用）
         await fs.mkdir(this._queueDir, {recursive: true});
@@ -87,17 +118,55 @@ export class PersistentQueue<T> {
         }
     }
 
+    /**
+     * キューの内容をクリア
+     */
     clear() {
         this._queue = [];
         [this._spos, this._epos] = [0, 0];
     }
 
+    /**
+     * キューファイルとマニフェストの最適化
+     */
+    async purge() {
+        const tmpQueueFileName = this._queueFile + '.tmp';
+        {
+            // sposからeposまでの内容をいったんtmpファイルに書き出して上書きリネーム
+            await using queueHandle = await fs.open(this._queueFile);
+            await using tmpQueueHandle = await fs.open(tmpQueueFileName, 'w');
+            await using queueStream = queueHandle.createReadStream({
+                start: this._spos,
+                end: this._epos
+            });
+            await using tmpQueueStream = tmpQueueHandle.createWriteStream();
+            
+            queueStream.pipe(tmpQueueStream);
+            
+            await finished(tmpQueueStream);
+        }
+
+        await fs.rename(tmpQueueFileName, this._queueFile);
+        // manifestファイルの更新
+        await this.writeManifest(0, this._epos - this._spos);
+        // メモリの更新
+        [this._spos, this._epos] = [0, this._epos - this._spos];
+    }
+
+    /**
+     * キューファイルの要素(改行区切りJSON)に変換
+     * @param {T} obj
+     */
     private toQueueFileEntry(obj: T) {
         // JSON文字列長 + １（改行分）増やす
         return JSON.stringify(obj) + '\n';
     }
 
-    // manifestをatomicに更新
+    /**
+     * マニフェストファイルに書き込み
+     * @param {number} spos 開始位置
+     * @param {number} epos 終了位置
+     */
     private async writeManifest(spos: number, epos: number) {
         let handle;
         try {
@@ -110,8 +179,11 @@ export class PersistentQueue<T> {
         await fs.rename(this._manifestFile + '.tmp', this._manifestFile);
     }
 
-    // manifestからデータ読み込み
-    private async readManifest() {
+    /**
+     * manifestからデータ読み込み
+     * @returns {Promise<number[]>} manifest
+     */
+    private async readManifest(): Promise<number[]> {
         const content = await fs.readFile(this._manifestFile, { encoding: 'utf-8' });
         return JSON.parse(content);
     }
@@ -120,7 +192,8 @@ export class PersistentQueue<T> {
         let handle;
         let stream;
         try {
-            handle = await fs.open(this._queueFile, 'r');
+            handle = await fs.open(this._queueFile, 'r+');
+            handle.truncate(this._epos);
             stream = handle.createReadStream({
                 start: this._spos,
                 end: this._epos
@@ -129,7 +202,6 @@ export class PersistentQueue<T> {
             let data = '';
             try {
                 for await (const chunk of stream) {
-
                     data += chunk;
                     // 読み込み完了した行はその場でパースして読み込む
                     const lines = data.split("\n");
